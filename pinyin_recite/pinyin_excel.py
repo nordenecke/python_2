e63e4d68b4ee8707602689d22d2a30185b66adfd
nordenecke <norden.liu@gmail.com>
2017-10-18 21:50:33 +0800

    20171018

--- pinyin_recite/pinyin_excel.py
+++ pinyin_recite/pinyin_excel.py
@@ -4,48 +4,94 @@ Spyder Editor
 
 This is a temporary script file.
 """
-source = load_cn_words('e:\文档\自动出题\词语默写.txt')
-#shuffle(source)  # 打乱顺序
 
-columns, rows = 15, 11  # 控制每页行列
-page_limit = 10  # 控制输出页数
-row_cur, page_cur = 1, 1
-def generator():
-    global source, columns, rows, row_cur, page_cur, page_limit
-    if not source or page_cur > page_limit:
+import os
+
+#import pypinyin
+#from pypinyin import pinyin, lazy_pinyin
+from pypinyin import pinyin
+
+###############################################################################
+
+class AssertError(Exception):
+    pass
+
+class StopError(Exception):
+    pass
+
+
+def ASSERT(condition):
+    if not condition:
+        raise AssertError()
+
+def STOP(message=''):
+    raise StopError(message)
+    
+def load_cn_words(fname):
+    ret=[]
+    print 'Chinese file path='+fname
+    if not os.path.isfile(fname):
+        STOP('file %s not exist!'% fname)
+    for l in open(fname,'r').read().decode(encoding='utf8').splitlines():
+        l = l.split('#')[0].strip()
+        if not l or l == '\ufeff':
+            continue
+        for w in l.split(' '):
+            if not w or w.startswith('~'):
+                continue
+            if w in ret:
+                continue
+            ret.append(w)
+    return ret
+
+###############################################################################
+import _winreg
+def get_desktop():
+    key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,\
+                          r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders',)
+    return _winreg.QueryValueEx(key, "Desktop")[0]
+
+
+def get_cn_word_file_path(filename):
+    desktop_path=get_desktop()
+    file_path = desktop_path+"\\"+filename
+    return file_path
+###############################################################################
+
+def generator(source):
+    if not source:
         STOP()
-    pinyin_lst, hanzi_lst, hanzi_blank_lst = [], [], []
+    pinyin_lst = []
+    hanzi_lst = []
     while True:
         if not source:
             break
         word = source.pop(0)
-        word1 = ' ' + word
-        hanzi_word = ''
-        if len(pinyin_lst) + len(word1) <= columns:
-            pinyin_lst += [i[0] for i in pinyin(word1)]
-            hanzi_lst += list(word)
-            hanzi_word +='('
-            j = 0
-            while j < len(word):
-                hanzi_word += ' '
-                j+=1
-            hanzi_word += ')'
-            hanzi_blank_lst+=list(hanzi_word)
-            if len(pinyin_lst) + 1 < columns:
-                pinyin_lst.append(' ')
-                hanzi_lst.append(' ')
-        else:
-            source.insert(0, word)
-            break
-    while len(pinyin_lst) < columns:
-        pinyin_lst.append(' ')
-        hanzi_lst.append(' ')
-    row_cur += 1
-    if row_cur > rows:
-        row_cur = 1
-        page_cur += 1
+        print 'Word='+word+'\n'
+        pinyin_lst += [i[0] for i in pinyin(word)]
+        hanzi_lst += word
     # 选择输出内容
     #return pinyin_lst + ['EOL'] + hanzi_lst  # 拼音+汉字
     #return ['EOL'] + hanzi_lst  # 仅汉字
     #return pinyin_lst + ['EOL']  # 仅拼音
-    return pinyin_lst + ['EOL'] + ['EOL'] + hanzi_blank_lst + ['EOL']   # 拼音+汉字空格 [老婆要求 :( ]
+    print 'pinyin_lst=\n'
+    print pinyin_lst
+    print 'hanzi_lst=\n'
+    print hanzi_lst
+    return pinyin_lst + ['EOL'] + hanzi_lst + ['EOL']   # 拼音+汉字空格 [老婆要求 :( ]
+
+
+def pinyin_generator():
+    source = load_cn_words(get_cn_word_file_path(u'Chinese_words.txt'))
+    print source
+    
+    columns, rows = 15, 11  # 控制每页行列
+    page_limit = 20  # 控制输出页数
+    generator(source)
+
+
+def main():
+    pinyin_generator()
+
+if __name__=="__main__":
+    main()
