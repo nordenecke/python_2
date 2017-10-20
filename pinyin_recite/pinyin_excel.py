66aea6b636e8e908c56b31089e304c05fa0c0401
Norden LIU <norden.liu@ericsson.com>
2017-10-20 17:08:10 +0800

    update filename

--- pinyin_recite/pinyin_excel.py
+++ pinyin_recite/pinyin_excel.py
@@ -7,9 +7,17 @@ This is a temporary script file.
 
 import os
 
+
 #import pypinyin
 #from pypinyin import pinyin, lazy_pinyin
 from pypinyin import pinyin
+
+from docx import Document
+
+###############################################################################
+input_word_list_file=r"Chinese_words.txt"
+output_docx_file=r"看拼音写汉字.doc"
+
 
 ###############################################################################
 
@@ -26,14 +34,15 @@ def ASSERT(condition):
 
 def STOP(message=''):
     raise StopError(message)
-    
+
 def load_cn_words(fname):
     ret=[]
     print 'Chinese file path='+fname
     if not os.path.isfile(fname):
         STOP('file %s not exist!'% fname)
     for l in open(fname,'r').read().decode(encoding='utf8').splitlines():
-        l = l.split('#')[0].strip()
+#        print l
+#        l = l.split('#')[0].strip()
         if not l or l == '\ufeff':
             continue
         for w in l.split(' '):
@@ -62,36 +71,67 @@ def generator(source):
     if not source:
         STOP()
     pinyin_lst = []
-    hanzi_lst = []
+#    hanzi_lst = []
     while True:
         if not source:
             break
         word = source.pop(0)
-        print 'Word='+word+'\n'
-        pinyin_lst += [i[0] for i in pinyin(word)]
-        hanzi_lst += word
+#        print '\nWord='+word
+#        print pinyin(word)
+#        pinyin_lst += [i[0] for i in pinyin(word)]
+        if word.startswith('#'):
+            pinyin_lst.append(word)
+        else:
+            pinyin_lst.append(pinyin(word))
+#        hanzi_lst.append(word)
+#        print 'word=['+word+']'
     # 选择输出内容
     #return pinyin_lst + ['EOL'] + hanzi_lst  # 拼音+汉字
     #return ['EOL'] + hanzi_lst  # 仅汉字
     #return pinyin_lst + ['EOL']  # 仅拼音
-    print 'pinyin_lst=\n'
-    print pinyin_lst
-    print 'hanzi_lst=\n'
-    print hanzi_lst
-    return pinyin_lst + ['EOL'] + hanzi_lst + ['EOL']   # 拼音+汉字空格 [老婆要求 :( ]
+#    print '\npinyin_lst='
+#    print pinyin_lst
+#    print '\nhanzi_lst='
+#    print hanzi_lst
+#    return pinyin_lst + ['EOL'] + hanzi_lst + ['EOL']   # 拼音+汉字空格 [老婆要求 :( ]
+    return pinyin_lst
 
 
 def pinyin_generator():
-    source = load_cn_words(get_cn_word_file_path(u'Chinese_words.txt'))
+    source = load_cn_words(get_cn_word_file_path(input_word_list_file))
+    print '\nsource='
     print source
-    
-    columns, rows = 15, 11  # 控制每页行列
-    page_limit = 20  # 控制输出页数
-    generator(source)
 
+#    columns, rows = 15, 11  # 控制每页行列
+#    page_limit = 20  # 控制输出页数
+    return generator(source)
+
+def export2doc(hzlst, pylst, export_file_name):
+    if len(hzlst)==0 || len(pylst)==0:
+        return False
+
+    #open document
+    document = Document()
+
+     #save file
+    desktop_path=get_desktop()
+    if os.path.exists(desktop_path+"\\"+export_file_name):
+        os.remove(desktop_path+"\\"+export_file_name)
+
+    document.save(desktop_path+"\\"+export_file_name)
+    return True
 
 def main():
-    pinyin_generator()
+    hzlst = load_cn_words(get_cn_word_file_path(input_word_list_file))
+    print '\nsource='
+    print hzlst
+
+#    columns, rows = 15, 11  # 控制每页行列
+#    page_limit = 20  # 控制输出页数
+    pylst=generator(hzlst)
+    print '\npylst='
+    print pylst
+    export2doc(hzlst,pylst,output_docx_file)
 
 if __name__=="__main__":
     main()
